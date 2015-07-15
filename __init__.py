b8a200876fd6978dc17ace6dcd908bd7f21fec6f
Brad Beimer <bbeimer@maxiglobalinc.com>
2015-07-14 21:33:13 -0700

    New Version

--- __init__.py
+++
@@ -1,8 +0,0 @@
-from .anidbmodel import AnidbModel, Anime, AnimeTitle, Setting
-from .anidbidsearch import AnidbIDSearch
-from .anidb import Anidb
-from .anidbcomm import AnidbComm
-
-#__all__ = ['Anidb', 'AnidbModel', 'AnidbIDSearch', 'AnidbComm', 'AnimeTitle', 'Anime', 'Settings']
-__all__ = ['Anidb', 'AnimeTitle', 'Anime', 'Settings']
-__version__ = "0.1.0"
